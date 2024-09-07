0275d294c9a48496c0ce3ff470c32c577a8e83e3
Andrii Bondar <andybondar3@gmail.com>
2024-09-07 11:15:12 +0100

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ const App = () => {
             ) : (
               <>
                 <List />
-                <Chat />
+                {chatId && <Chat />}
               </>
             )}
           </>
