473b20096729ab27aca9f4b5f7a229346ccb65bf
Andrii Bondar <andybondar3@gmail.com>
2024-09-22 17:27:54 +0100

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import Chat from "./components/chat/chat"
+import Chat from "./components/chat/Chat"
 import List from "./components/list/List"
 import Login from './components/login/Login'
 import Notification from "./components/notification/Notification"
